caa6ea226b98f1bab977c50df07e238c67de2b89
Richard <richwats@cisco.com>
2021-12-09 11:26:16 +1100

    test2

--- templates.tf
+++ templates.tf
@@ -4,7 +4,7 @@ resource "mso_schema_template" "segments" {
   for_each = var.segments
 
   schema_id       = mso_schema.schema.id
-  name            = lower(each.value.name) ## Enforced lower
+  name            = each.value.name
   display_name    = each.value.display_name
   tenant_id       = data.mso_tenant.tenant.id
 }
