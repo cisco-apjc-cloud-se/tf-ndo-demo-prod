b5af94a425a0af37e0656187206da99b46aae99d
Richard <richwats@cisco.com>
2021-12-13 19:39:46 +1100

    new test

--- modules/ndo/deploy.tf
+++ modules/ndo/deploy.tf
@@ -9,31 +9,32 @@ locals {
 }
 
 locals {
-  // mergedsites =  merge(local.cloudsitemap, local.acisitemap)
-  mergedsites = flatten(merge(local.cloudsitemap, local.acisitemap))
+  mergedsites =  merge(local.cloudsitemap, local.acisitemap)
 }
 
 
 ### Deploy Trigger ###
 resource "mso_schema_template_deploy" "deploy" {
   // for_each = var.segments
-  for_each = var.undeploy == false ? var.segments : {}
+  for_each = var.undeploy == false ? var.segments : local.mergedsites
 
   schema_id       = mso_schema.schema.id
   template_name   = each.value.name
+  site_id         = try(data.mso_site.sites[each.value.site_name].id, "unspecified")
+  undeploy        = var.undeploy
 
   depends_on = [mso_schema_site_vrf_region.region, mso_schema_template_bd_subnet.subnet]
 }
 
-### Undeploy Trigger ####
-resource "mso_schema_template_deploy" "undeploy" {
-  // for_each = toset( var.undeploy == true ? flatten(merge(local.cloudsitemap, local.acisitemap)) : [] )
-  for_each = var.undeploy == true ? local.mergedsites : {}
-
-  schema_id       = mso_schema.schema.id
-  template_name   = each.value.segment_name
-  site_id         = data.mso_site.sites[each.value.site_name].id
-  undeploy        = true
-
-  depends_on = [mso_schema_site_vrf_region.region, mso_schema_template_bd_subnet.subnet]
-}
+// ### Undeploy Trigger ####
+// resource "mso_schema_template_deploy" "undeploy" {
+//   // for_each = toset( var.undeploy == true ? flatten(merge(local.cloudsitemap, local.acisitemap)) : [] )
+//   for_each = var.undeploy == true ? local.mergedsites : {}
+//
+//   schema_id       = mso_schema.schema.id
+//   template_name   = each.value.segment_name
+//   site_id         = data.mso_site.sites[each.value.site_name].id
+//   undeploy        = true
+//
+//   depends_on = [mso_schema_site_vrf_region.region, mso_schema_template_bd_subnet.subnet]
+// }
